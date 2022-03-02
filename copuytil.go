e095fa65649a5d0fb5d86782180384b62453c1c9
NsTy <zhelnovaanastasiya22@gmail.com>
2022-03-02 23:02:58 +0700

    init commitg

--- copuytil.go
+++ copuytil.go
@@ -1,7 +1,24 @@
 package copyutil
 
-import "fmt"
+import (
+	"os"
+	"io"
+	"github.com/cheggaaa/pb/v3"
+)
 
-func Hello()  {
-	fmt.Println("hello")
+func Copy(from string, to string, limit int, offset int) error {
+	fileFrom, err := os.Open(from)
+	fileFrom.Seek(offset, io.SeekStart)
+	fileTo, err := os.Open(to)
+	bar := pb.StartNew(count)
+	_, err = io.CopyN(fileTo, fileFrom, limit)
+	for i := 0; i < count; i++ {
+		bar.Increment()
+		time.Sleep(time.Millisecond)
+	}
+	bar.Finish()
+	if err != nil{
+		return err
+	}
+	return nil
 }
